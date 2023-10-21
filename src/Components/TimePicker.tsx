84db636408f048d231e18b871650f60a8351aebf
catonnet <julien.catonnet@essec.edu>
2023-10-21 11:48:53 +0100

    fix datepicker for android by adding conitional rendering and native props

--- src/Components/TimePicker.tsx
+++ src/Components/TimePicker.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { SafeAreaView} from "react-native";
 import DateTimePicker from '@react-native-community/datetimepicker';
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 export const SelectTimePicker = ({onChange, date}: any) => {
 
     return (
       <SafeAreaView>
-          <DateTimePicker
+          <RNDateTimePicker
             testID="dateTimePicker"
             value={date}
             mode='time'
--- src/Pages/DateTimePage.tsx
+++ src/Pages/DateTimePage.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, Platform } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { setStartTime, setEndTime } from '../utils/formDataSlice';
 import { SelectTimePicker } from '../Components/TimePicker';
 import { AppDispatch } from '../utils/store';
 import { setByTimezone } from '../utils/dateTransformer';
-
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export const DateTimePage = ({ navigation }: any) => {
   const dispatch: AppDispatch = useDispatch();
@@ -14,9 +14,26 @@ export const DateTimePage = ({ navigation }: any) => {
   const nowAndFourHoursLater = new Date(now.getTime() + 4 * 60 * 60 * 1000);  
   const [dateTimeStart, setDateTimeStart] = useState(now);
   const [dateTimeEnd, setDateTimeEnd] = useState(nowAndFourHoursLater);
+  const [showStartPicker, setShowStartPicker] = useState(false);
+  const [showEndPicker, setShowEndPicker] = useState(false);
 
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
+  const formatTime = (date: Date) => {
+    return `${date.getHours()}:${("0" + date.getMinutes()).slice(-2)}`;
+  };
+
+  const showStartTimePicker = () => {
+    if (Platform.OS === 'android') {
+      setShowStartPicker(true);
+    }
+}
+
+const showEndTimePicker = () => {
+  if (Platform.OS === 'android') {
+    setShowEndPicker(true);
+  }
+};
 
   const handleNext = () => {
     if (dateTimeEnd <=  dateTimeStart) {
@@ -41,29 +58,76 @@ export const DateTimePage = ({ navigation }: any) => {
     setDateTimeEnd(currentDateEnd);
   };
 
-  return (
-    <View style={styles.pageContainer}>
-
-      <View style={styles.info}>
-        <Text style={styles.subtitle}> We will meet at: (if I am not late...) </Text>
-        <SelectTimePicker onChange={onChangeStart} date={dateTimeStart} ></SelectTimePicker>
-      </View>
-
-      <View style={styles.info}>
-        <Text style={styles.subtitle}> I plan to go back home at: (if we stick to the plan...) </Text>
-        <SelectTimePicker onChange={onChangeEnd} date={dateTimeEnd} ></SelectTimePicker>
-      </View>
-      {errorMessage && (
-        <View style={styles.errorContainer}>
-          <Text style={styles.errorText}>{errorMessage}</Text>
+    return (
+      <View style={styles.pageContainer}>
+        <View style={styles.info}>
+          <Text style={styles.subtitle}> We will meet at: (if I am not late...) </Text>
+            {Platform.OS === 'ios' ? (
+              <SelectTimePicker onChange={onChangeStart} date={dateTimeStart} />
+            ) : (
+              <>
+            <TouchableOpacity onPress={showStartTimePicker}>
+              <View style={styles.timeTextContainer}>
+                <Text style={styles.timeText}>{formatTime(dateTimeStart)}</Text>
+              </View>
+            </TouchableOpacity>
+              {showStartPicker && (
+                <DateTimePicker
+                  mode="time"
+                  display="default"
+                  value={dateTimeStart}
+                  onChange={(event, selectedDate) => {
+                    setShowStartPicker(false);
+                    if (selectedDate) {
+                      setDateTimeStart(selectedDate);
+                    }
+                  }}
+                />
+              )}
+            </>
+          )}
         </View>
-      )}
-
-      <TouchableOpacity style={styles.button} onPress={handleNext}>
-        <Text style={styles.buttonText}>Next</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    
+        <View style={styles.info}>
+          <Text style={styles.subtitle}> I plan to go back home at: (if we stick to the plan...) </Text>
+          {Platform.OS === 'ios' ? (
+            <SelectTimePicker onChange={onChangeEnd} date={dateTimeEnd} />
+          ) : (
+            <>
+              <TouchableOpacity onPress={showEndTimePicker}>
+                <View style={styles.timeTextContainer}>
+                  <Text style={styles.timeText}>{formatTime(dateTimeEnd)}</Text>
+                </View>
+              </TouchableOpacity>
+              {showEndPicker && (
+                <DateTimePicker
+                  mode="time"
+                  display="default"
+                  value={dateTimeEnd}
+                  onChange={(event, selectedDate) => {
+                    setShowEndPicker(false);
+                    if (selectedDate) {
+                      setDateTimeEnd(selectedDate);
+                    }
+                  }}
+                />
+              )}
+            </>
+          )}
+        </View>
+        
+        {errorMessage && (
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>{errorMessage}</Text>
+          </View>
+        )}
+    
+        <TouchableOpacity style={styles.button} onPress={handleNext}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    );
+    
 };
 
 
@@ -78,13 +142,25 @@ const styles = StyleSheet.create({
       fontWeight: 'bold',
       marginBottom: 30,
       color: '#29B7AE',
-      textAlign: 'left',  // This aligns the text to the left
-      width: '100%',      // This ensures the text occupies the full width
+      textAlign: 'left',
+      width: '100%',
   },
   info: {
       alignSelf: 'stretch',
-      alignItems: 'center',  // This centers the TimePicker
+      alignItems: 'center',
       marginBottom: 50,
+  },
+  timeTextContainer: {
+    backgroundColor: '#E0E0E0', 
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    marginTop: 10,
+    alignItems: 'center'
+  },
+  timeText: {
+    color: '#333',
+    fontSize: 16
   },
   errorText: {
     color: 'red',
